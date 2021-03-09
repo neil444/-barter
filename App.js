c6b3fa415354675b81d47fd398db7a15e68d7c4e
neil444 <66010589+neil444@users.noreply.github.com>
2021-03-09 16:13:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screen/welcomescreen';
+
+export default function App() {
+  return (
+    <WelcomeScreen/>
+  );
+}
